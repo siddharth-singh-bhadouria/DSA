b9a7c594631fe5a44ad2809eaa2110b1a52606f1
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-12-25 03:02:42 +0530

    Practise question 2

--- practise.cpp
+++ practise.cpp
@@ -1,20 +1,42 @@
+// 1.
+// #include <iostream>
+// using namespace std;
+
+// void func(int i, int n)
+// {
+//     if (i > n)
+//     {
+//         return;
+//     }
+//     func(i + 1, n);
+//     cout << i << endl;
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     func(1, n);
+//     return 0;
+// }
+
+// 2.
 #include <iostream>
 using namespace std;
 
-void func(int i, int n)
+bool func(int i, string &s)
 {
-    if (i > n)
-    {
-        return;
-    }
-    func(i + 1, n);
-    cout << i << endl;
+    if (i >= s.size() / 2)
+        return true;
+    if (s[i] != s[s.size() - i - 1])
+        return false;
+    return func(i + 1, s);
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    func(1, n);
+    string s;
+    cin >> s;
+    cout << func(0, s) << endl;
     return 0;
 }
